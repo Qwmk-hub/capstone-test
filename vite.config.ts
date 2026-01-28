807e8684d7fd148ccb74550d5df095b233bf74a6
PARK SUNGCHEOL <qwmk1025@hanyang.ac.kr>
2026-01-28 17:49:29 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/capstone-test/',
+  base: '/',
   server: { port: 5173 },
   build: {
     outDir: 'docs',
